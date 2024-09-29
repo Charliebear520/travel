3d3ec66d1865eb2df0fb44ef6b6e160875a0c62d
hueylu910201 <hueylu910201@gmail.com>
2024-09-29 11:11:58 +0800

    Day16&17-install Leaflet&custom marker

--- src/components/MapItem/MapItem.jsx
+++ src/components/MapItem/MapItem.jsx
@@ -1,9 +1,40 @@
 import styles from "./map.module.css"
+import { Row, Col } from 'antd';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';  // 引入 Leaflet 的樣式
+import landmarks from "../../json/place.json"
 
 export default function MapItem() {
+    const center =[36.23541690015412, 137.97220383903155];
+    const markerIcon = new L.Icon({
+        iconUrl: "/public/pin.png",
+        iconSize: [35, 35],
+      });
+
     return (
-        <>
-            <h1>This is Map page</h1>
-        </>
-    )
+        
+        <div className={styles.fullScreen}>
+            <Row className={styles.mapContainer}>
+                <Col span={24} className={styles.mapCol}>
+                    <MapContainer center={center} zoom={7} className={styles.map}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        />
+                        <Marker position={center} >
+                            <Popup>
+                                地圖中心
+                            </Popup>
+                        </Marker>
+                        {/* 從 landmarks 中匯入的 Marker */}
+                        {landmarks.map((landmark) => (
+                            <Marker key={landmark.id} position={landmark.position} icon={markerIcon}>
+                                <Popup>{landmark.name}</Popup>
+                            </Marker>
+                        ))}
+                    </MapContainer>
+                </Col>
+            </Row>
+        </div>
+    );
 }
