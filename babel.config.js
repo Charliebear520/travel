b6030180873b4ccaed0102669e7e7b43433d17e1
hueylu910201 <hueylu910201@gmail.com>
2024-10-07 00:20:28 +0800

    fixbug-babelconfig2

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
-module.exports = {
+export default {
     presets: [
-      '@babel/preset-env', // 支持 ES6+
-      '@babel/preset-react' // 支持 JSX
-    ],
-    plugins: [
-      // 在這裡可以添加其他插件
+      '@babel/preset-env',
+      '@babel/preset-react'
     ]
   };
