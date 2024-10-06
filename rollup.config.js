8c563feb42ac9bf647b933424b452ba564cfa9fd
hueylu910201 <hueylu910201@gmail.com>
2024-10-06 23:45:41 +0800

    fixbug-npm build

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+  ],
+};
