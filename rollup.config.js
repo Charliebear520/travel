3842d2fb23864a17276056134956b9c48b1367a3
hueylu910201 <hueylu910201@gmail.com>
2024-10-06 23:55:28 +0800

    fixbug-rollup2

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/main.jsx',
   output: {
     file: 'dist/bundle.js',
     format: 'esm',
