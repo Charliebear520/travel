79fe1d8ccdd642d76e3e174171d76790b54bc14e
hueylu910201 <hueylu910201@gmail.com>
2024-10-07 10:57:15 +0800

    fixbug-vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import copy from 'rollup-plugin-copy';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [
-        { src: 'public/_redirects', dest: 'dist' }, // 複製 _redirects 文件到 dist 目錄
-      ],
-      verbose: true,
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-  },
-});
+  plugins: [react()],
+})
