87ab62231958b8d5333c6e0831538fe1eb313c8c
hueylu910201 <hueylu910201@gmail.com>
2024-10-07 10:46:31 +0800

    fixbug-rollup-plugin-copy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'public/_redirects', dest: 'dist' }, // 複製 _redirects 文件到 dist 目錄
+      ],
+      verbose: true,
+    }),
+  ],
   build: {
     outDir: 'dist',
   },
-})
+});
